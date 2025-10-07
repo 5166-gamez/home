7651ff075784728a31b248b1284b9111f0df14f0
5166-gamez <5166games@gmail.com>
2025-10-06 22:07:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -33,30 +33,26 @@ function loadSection(section) {
 }
 
 function loadGames() {
-  // Array of your embedded games
   const games = [
-    {
-      name: "Slope",
-      embed: `<iframe src="https://slopegame.lol/play/slope" width="800" height="600" frameborder="0" allowfullscreen></iframe>`
-    },
-    {
-      name: "Run 3",
-      embed: `<iframe src="https://player03.com/run/3/beta" width="800" height="600" frameborder="0" allowfullscreen></iframe>`
-    },
-    {
-      name: "Cookie Clicker",
-      embed: `<iframe src="https://cookieclicker.ee" width="800" height="600" frameborder="0" allowfullscreen></iframe>`
-    }
+    { name: "Slope", embed: `<iframe src='https://slopegame.lol/play/slope' width='800' height='600' frameborder='0' allowfullscreen></iframe>` },
+    { name: "Run 3", embed: `<iframe src='https://player03.com/run/3/beta' width='800' height='600' frameborder='0' allowfullscreen></iframe>` },
+    { name: "Cookie Clicker", embed: `<iframe src='https://cookieclicker.ee' width='800' height='600' frameborder='0' allowfullscreen></iframe>` }
   ];
 
-  // Build a grid of game buttons
   content.innerHTML = `
     <h2>Games</h2>
-    <div id="game-list">
-      ${games.map(g => `<button onclick="showGame(\`${g.embed}\`)">${g.name}</button>`).join("")}
-    </div>
+    <div id="game-list"></div>
     <div id="game-frame" style="margin-top:20px;"></div>
   `;
+
+  const list = document.getElementById("game-list");
+
+  games.forEach(g => {
+    const btn = document.createElement("button");
+    btn.textContent = g.name;
+    btn.addEventListener("click", () => showGame(g.embed));
+    list.appendChild(btn);
+  });
 }
 
 function showGame(embedHTML) {
